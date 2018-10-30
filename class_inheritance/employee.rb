7c1ac7767c3053141bf769a8dbacb08ce10756dc
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-10-29 18:08:26 -0700

    summit work at end-of-day

--- class_inheritance/employee.rb
+++ class_inheritance/employee.rb
@@ -12,23 +12,29 @@ class Employee
   def bonus(multiplier)
     salary * multiplier
   end
+
+  def assign_boss(boss)
+    @boss = boss
+    boss.subordinates << self
+  end
 end
 
 class Manager < Employee
+  attr_accessor :subordinates
 
-  def initialize(name, title, salary, subordinates)
+  def initialize(name, title, salary)
     super(name, title, salary)
-    @subordinates = subordinates
+    @subordinates = []
   end
 
-  def assign_boss
-    @subordinates.each do |ele|
-      ele.boss = self
-    end
-  end
+  # def assign_boss
+  #   @subordinates.each do |ele|
+  #     ele.boss = self
+  #   end
+  # end
 
   def bonus(multiplier)
-    @subordinates.reduce(0){|total_sub_salary,el| total_sub_salary += el.salary}
+    total_sub_salary = @subordinates.reduce(0){|total_sub_salary,el| total_sub_salary += el.salary}
 
     total_sub_salary * multiplier
   end
